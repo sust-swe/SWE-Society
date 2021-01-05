85f64767a65ba1b31c36526edda070c1d4dc3711
RakibulRanak <rakibulhasanranak1@gmail.com>
2021-01-06 02:46:37 +0600

    postgresql database connection implemented

--- server/server.js
+++ server/server.js
@@ -1,14 +1,29 @@
 'use strict';
 
 // Importing packages
-
+const {Client} = require('pg')
 
 // Confuguring the environment variables
 require('dotenv').config();
 const PORT = process.env.PORT || 8000;
 const HOST = process.env.HOST || 'localhost';
 
+
 // Database Connection
+
+console.log(process.env.DB_USER);
+const client = new Client({
+  user:process.env.DB_USER,
+  password:process.env.DB_PASS,
+  host:process.env.DB_HOST,
+  port:process.env.DB_PORT,
+  database:process.env_DB
+})
+
+client.connect()
+.then(()=>console.log("Connected"))
+.catch(e=>console.log(e))
+.finally(()=>client.end())
 
 // Importing the express app
 const app = require('./app');
