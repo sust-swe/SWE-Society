0c3f56fcf8e8512d513a50a7d9941a4d275d2668
RakibulRanak <rakibulhasanranak1@gmail.com>
2021-01-10 04:36:08 +0600

    error handling and bug fixing started

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -13,152 +13,127 @@ const sendEmail = require('./../utils/sendEmail');
 
 
 exports.register = catchAsync(async (req, res, next) => {
-    const {email,reg_no,name} = req.body;
-    const user = await client.query(`SELECT * FROM member where reg_no=${reg_no};`);
-    if(user.rows.length>0)
-        return res.status(401).json("User Already Exist!");
-    else
-    {
-        const password = generator.generate({
-            length: 10,
-            numbers: true
-        });
-        //send this password to the emaail of the new user
-        const message = `<div>Hey ${name}, Your account is created for Swe Society.Your first time password is <h1>${password}</h1><br> 
-                          Please change this password after first login.</div>`;
-      
-        sendEmail(email,'Greetings from Swe Society',message);
-        const salt = await bcrypt.genSalt(10);
-        const bcryptPassword = await bcrypt.hash(password,salt);
-        const batch = reg_no.substring(0, 4);
-        const now = new Date().toUTCString();
-        const newUser = await client.query(
-            `INSERT INTO member (name,email,password,reg_no,batch,last_password_changed_at) VALUES 
-            ('${name}','${email}','${bcryptPassword}',${reg_no},${batch},'${now}') RETURNING *;`
-        );
-       
-        res.json({
-            pass : password,
-            hpass: bcryptPassword
-        });
-    }
+  const { email, reg_no, name } = req.body;
+  const user = await client.query(`SELECT * FROM member where reg_no=${reg_no};`);
+  if (user.rows.length > 0)
+    return res.status(401).json("User Already Exist!");
+
+  const password = generator.generate({
+    length: 10,
+    numbers: true
   });
-  
+  //send this password to the emaail of the new user
+  const message = `<div>Hey ${name}, Your account is created for Swe Society.Your first time password is <h1>${password}</h1><br> 
+                          Please change this password after first login.</div>`;
+
+  sendEmail(email, 'Greetings from Swe Society', message);
+  const salt = await bcrypt.genSalt(10);
+  const bcryptPassword = await bcrypt.hash(password, salt);
+  const batch = reg_no.substring(0, 4);
+  const now = new Date().toUTCString();
+  const newUser = await client.query(
+    `INSERT INTO member (name,email,password,reg_no,batch,last_password_changed_at) VALUES 
+            ('${name}','${email}','${bcryptPassword}',${reg_no},${batch},'${now}') RETURNING *;`
+  );
+
+  res.json({
+    pass: password,
+    hpass: bcryptPassword
+  });
+
+});
+
 
 exports.login = catchAsync(async (req, res, next) => {
-    const { email, password } = req.body;
-    const user = await client.query(`SELECT * FROM member WHERE email='${email}';`);
-    if(user.rows.length==0)
-         return res.status(401).json("Invalid Email");
-    const validPass= await bcrypt.compare(password,user.rows[0].password);
-    if(!validPass)
-        return res.status(401).json("Wrong Password");
-  
-   // console.log(user.rows[0].reg_no,process.env.jwtSessionTokenExpire);
-    const jwtToken = jwtGenerator(user.rows[0].reg_no,process.env.jwtSessionTokenExpire);
-    return res.json({token:jwtToken});
+  const { email, password } = req.body;
+  const user = await client.query(`SELECT * FROM member WHERE email='${email}';`);
+  if (user.rows.length == 0)
+    return res.status(401).json("Invalid Email");
+  const validPass = await bcrypt.compare(password, user.rows[0].password);
+  if (!validPass)
+    return res.status(401).json("Wrong Password");
+
+  // console.log(user.rows[0].reg_no,process.env.jwtSessionTokenExpire);
+  const jwtToken = jwtGenerator(user.rows[0].reg_no, process.env.jwtSessionTokenExpire);
+  return res.json({ token: jwtToken });
 });
 
 exports.forgotPassword = catchAsync(async (req, res, next) => {
-    // 1) Get user based on POSTed email
-    const {email} = req.body;
-    const user = await client.query(`SELECT * FROM member WHERE email='${email}';`);
-    if(user.rows.length==0)
-         return res.status(401).json("No user found");
+  // 1) Get user based on POSTed email
+  const { email } = req.body;
+  const user = await client.query(`SELECT * FROM member WHERE email='${email}';`);
+  if (user.rows.length == 0)
+    return res.status(401).json("No user found");
 
-    const password = generator.generate({
-        length: 10,
-        numbers: true
-    });
-    const jwtToken = jwtGenerator(user.rows[0].reg_no,process.env.jwtResetTokenExpire);
-    const url = `https://localhost:8000/resetpassword/${jwtToken}`;
-    const message = `<h3>Hey ${user.rows[0].name},Click here and reset your password within 5 minutes</h3>
+  const password = generator.generate({
+    length: 10,
+    numbers: true
+  });
+  const jwtToken = jwtGenerator(user.rows[0].reg_no, process.env.jwtResetTokenExpire);
+  const url = `https://localhost:8000/resetpassword/${jwtToken}`;
+  const message = `<h3>Hey ${user.rows[0].name},Click here and reset your password within 5 minutes</h3>
                     <p>${url}</p>`;
-    sendEmail(email,'Reset Password',message);
-    res.send("Reset Token Sent to email");
+  sendEmail(email, 'Reset Password', message);
+  res.send("Reset Token Sent to email");
 });
 
 exports.resetPassword = catchAsync(async (req, res, next) => {
-    const token = req.params.token;
-    const decoded = await promisify(jwt.verify)(token, process.env.jwtSecret);
-    const currentUser = decoded.user;
-    console.log(currentUser);
-    const salt = await bcrypt.genSalt(10);
-    const bcryptPassword = await bcrypt.hash(req.body.password,salt);
-    const now = new Date().toUTCString();
-    await client.query(`Update member set password='${bcryptPassword}',last_password_changed_at='${now}' where reg_no=${currentUser.id}`);
-    res.send("Password Resetted");
-  
+  const token = req.params.token;
+  const decoded = await promisify(jwt.verify)(token, process.env.jwtSecret);
+  const currentUser = decoded.user;
+  console.log(currentUser);
+  const salt = await bcrypt.genSalt(10);
+  const bcryptPassword = await bcrypt.hash(req.body.password, salt);
+  const now = new Date().toUTCString();
+  await client.query(`Update member set password='${bcryptPassword}',last_password_changed_at='${now}' where reg_no=${currentUser.id}`);
+  res.send("Password Resetted");
+
 });
 
 exports.updatePassword = catchAsync(async (req, res, next) => {
-    const {oldPass,newPass} = req.body;
-    console.log(oldPass,newPass,req.user);
-    const user = await client.query(`SELECT password FROM member WHERE reg_no=${req.user.reg_no};`);
-    const realPass = user.rows[0].password;
-    const truePass= await bcrypt.compare(oldPass,realPass);
-    if(!truePass)
-         return res.status(401).json("Wrong Password");
-    const salt = await bcrypt.genSalt(10);
-    const bcryptPassword = await bcrypt.hash(newPass,salt);
-    const now = new Date().toUTCString();
-    await client.query(`Update member set password='${bcryptPassword}',last_password_changed_at='${now}' where reg_no=${req.user.reg_no}`);
-    const jwtToken = jwtGenerator(req.user.reg_no,process.env.jwtSessionTokenExpire);
-    return res.json({token:jwtToken}); 
+  const { oldPass, newPass } = req.body;
+  console.log(oldPass, newPass, req.user);
+  const user = await client.query(`SELECT password FROM member WHERE reg_no=${req.user.reg_no};`);
+  const realPass = user.rows[0].password;
+  const truePass = await bcrypt.compare(oldPass, realPass);
+  if (!truePass)
+    return res.status(401).json("Wrong Password");
+  const salt = await bcrypt.genSalt(10);
+  const bcryptPassword = await bcrypt.hash(newPass, salt);
+  const now = new Date().toUTCString();
+  await client.query(`Update member set password='${bcryptPassword}',last_password_changed_at='${now}' where reg_no=${req.user.reg_no}`);
+  const jwtToken = jwtGenerator(req.user.reg_no, process.env.jwtSessionTokenExpire);
+  return res.json({ token: jwtToken });
 });
-  
+
 exports.protect = catchAsync(async (req, res, next) => {
-    // 1) Getting token and check of it's there
-    let token;
-    if (
-      req.headers.authorization &&
-      req.headers.authorization.startsWith('Bearer')
-    ) {
-      token = req.headers.authorization.split(' ')[1];
-    }
-  
-    if (!token) {
-      return next(
-        new AppError('You are not logged in! Please log in to get access.', 401)
-      );
-    }
-    // 2) Verification token
-    const decoded = await promisify(jwt.verify)(token, process.env.jwtSecret);
-  
-    // 3) Check if user still exists
-    const currentUser = decoded.user;
-    if (!currentUser) {
-      return next(
-        new AppError(
-          'The user belonging to this token does no longer exist.',
-          401
-        )
-      );
-    }
-    const user = await client.query(
-        `SELECT reg_no,role,last_password_changed_at FROM member WHERE reg_no = ${currentUser.id}`);
-   // console.log(decoded.iat);
-    const changedTimestamp = parseInt(
-      user.rows[0].last_password_changed_at / 1000,
-      10
-    );
-    if(changedTimestamp>decoded.iat){
-       return next(
-         new AppError('Your password changed recently.Please log in again', 401)
-    );
-    }
-    req.user = user.rows[0];
-    next();
+  // 1) Getting token and check of it's there
+  let token;
+  if (req.headers.authorization && req.headers.authorization.startsWith('Bearer'))
+    token = req.headers.authorization.split(' ')[1];
+  if (!token)
+    return next(new AppError('You are not logged in! Please log in to get access.', 401));
+  // 2) Verification token
+  const decoded = await promisify(jwt.verify)(token, process.env.jwtSecret);
+  const currentUser = decoded.user;
+  // 3) Check if user still exists
+  const user = await client.query(
+    `SELECT reg_no,role,last_password_changed_at FROM member WHERE reg_no = ${currentUser.id}`);
+  if (user.rows.length == 0)
+    return next(new AppError('The user belonging to this token does no longer exist.', 401));
+  const changedTimestamp = parseInt(user.rows[0].last_password_changed_at / 1000, 10);
+  if (changedTimestamp > decoded.iat)
+    return next(new AppError('Your password changed recently.Please log in again', 401));
+
+  req.user = user.rows[0];
+  next();
 });
 
 exports.restrictTo = (...roles) => {
-    return (req, res, next) => {
-      // roles ['admin', 'moderator']
-      if (!roles.includes(req.user.role)) {
-        return next(
-          new AppError('You do not have permission to perform this action', 403)
-        );
-      }
-      next();
-    };
+  return (req, res, next) => {
+    // roles ['admin', 'moderator']
+    if (!roles.includes(req.user.role))
+      return next(new AppError('You do not have permission to perform this action', 403));
+    next();
+  };
 };
--- server/utils/appError.js
+++ server/utils/appError.js
@@ -1,12 +1,12 @@
 'use strict';
 
 class AppError extends Error {
-  constructor(message, statusCode, error) {
+  constructor(message, statusCode) {
     super(message);
 
     this.statusCode = statusCode;
     this.status = `${statusCode}`.startsWith('4') ? 'fail' : 'error';
-    this.error = error;
+    // this.error = error;
     this.isOperational = true;
 
     Error.captureStackTrace(this, this.constructor);
