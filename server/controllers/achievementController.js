4c1dc28edce9092f4b282f9a2f54574f3c109f74
mhRumi <rumiswe@gmail.com>
2021-01-19 01:20:12 +0600

    fix

--- server/controllers/achievementController.js
+++ server/controllers/achievementController.js
@@ -1,35 +1,40 @@
 const catchAsync = require('./../utils/catchAsync');
 const client = require('../db');
+const Achievement = require('../models/AchievementModel');
+const AppError = require('../utils/appError');
 
 
 exports.getAllAchievement = catchAsync(async(req, res, next) => {
-    const result = await client.query(`Select * From achievements`);
-    res.send(result.rows);
+    const achievements = await Achievement.findAll();
+
+    if(achievements == null)
+        return next(new AppError(`Does not found`, 404));
+   res.status(200).json(achievements);
 });
 
 exports.getSingleAchievement = catchAsync(async(req, res, next) => {
-    const result = await client.query(`Select * From achievements where achievement_id=${req.params.achievement_id}`);
-    res.send(result.rows);
+    const achievement = await Achievement.findOne({where:{id: req.params.id}})
+    if(achievement == null)
+        return next(new AppError(`Does not found`, 404));
+   res.status(200).json(achievement);
 });
 
 exports.getSpecificUserAchievements = catchAsync(async(req, res, next) => {
-    const result = await client.query(`Select * From achievements where user_id=${req.params.user_id}`);
-    res.send(result.rows);
+    const achievements = await Achievement.findAll({where: {reg_no: req.params.reg_no}});
+    if(achievements == null)
+        return next(new AppError(`Does not found`, 404));
+    res.status(200).json(achievements)
 })
 
 exports.addAchievement = catchAsync(async(req, res , next) => {
-
-    const query = {
-        text: `INSERT INTO achievements(team_name, title, position, date, image_url, team_members, description, user_id)
-        VALUES($1, $2, $3, $4, $5, $6,$7, $8) RETURNING * ;`,
-        values: [req.body.team_name, req.body.title, req.body.position, req.body.date,req.body.image_url,
-             req.body.team_members, req.body.description, req.body.user_id ]
-    }
-    const result = await client.query(query);
-    res.send(result.rows);
+    req.body.reg_no = req.user.reg_no;
+    const achievement = await Achievement.create(req.body);
+    res.status(200).json(achievement);
 });
 
 exports.deleteAchievement = catchAsync(async(req, res, next) => {
-    const result = await client.query(`Delete From achievements where achievement_id=${req.params.achievement_id};`);
-    res.send("Successfully deleted");
+    const achievement = await Achievement.destroy({where:{id: req.params.id, reg_no: req.user.reg_no}})
+    if(achievement == 0)
+        return next(new AppError(`Does not found`, 404));
+    res.status(200).json({message:"Successfully deleted", achievement});
 });
--- server/controllers/gallaryController.js
+++ server/controllers/gallaryController.js
@@ -1,34 +1,39 @@
 const catchAsync = require('./../utils/catchAsync');
 const client = require('../db');
+const Gallary = require('../models/GallaryModel');
+const AppError = require('../utils/appError');
 
 
 exports.addContent = catchAsync(async(req, res, next) => {
-    const query = {
-        text: `Insert Into gallary(imageurl, date, caption)
-        VALUES('${req.body.imageurl}', '${req.body.date}', '${req.body.caption}') RETURNING *`
-    }
-
-    const result = await client.query(query);
-
-    res.send(result.rows);
+    const content = await Gallary.create(req.body);
+    res.status(200).json(content);
 });
 
 exports.deleteContent = catchAsync(async(req, res, next) => {
-    const result = await client.query(`Delete From gallary where image_id=${req.params.id};`);
-    res.send("Succefully deleted");
+    const result = await Gallary.destroy({where: {id: req.params.id}});
+
+    if(!result)
+        return next(new AppError(`Does not found`, 404));
+    res.status(200).json({message: "Succefully deleted"});
 });
 
 exports.updateContent = catchAsync(async(req, res , next) => {
-    const result = await client.query(`Update gallary set caption='${req.body.caption}' where image_id = ${req.params.id} RETURNING *;`);
-    res.send(result.rows);
+    const content = await Gallary.update(req.body,{returning: true,where: {id: req.params.id}})
+    if(content[0] == 0)
+        return next(new AppError(`Does not found`, 404));
+    res.status(200).json(content);
 });
 
 exports.getALLContent = catchAsync(async(req, res, next) => {
-    const result = await client.query('Select * From gallary;');
-    res.send(result.rows);
+    const contents = await Gallary.findAll();
+    if(contents == null)
+        return next(new AppError(`Does not found`, 404));
+    res.status(200).json(contents);
 });
 
 exports.getSingleContent = catchAsync(async(req, res, next) => {
-    const result = await client.query(`Select * From gallary where image_id=${req.params.id};`);
-    res.send(result.rows);
+    const content = await Gallary.findOne({where: {id: req.params.id}});
+    if(content == null)
+        return next(new AppError(`Does not found`, 404));
+    res.status(200).json(content);
 });
