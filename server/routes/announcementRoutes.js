fa019c504263fa20b7551d12f06efe0e3d62bfc3
RakibulRanak <rakibulhasanranak1@gmail.com>
2021-02-07 19:21:47 +0600

    letter

--- server/routes/announcementRoutes.js
+++ server/routes/announcementRoutes.js
@@ -3,9 +3,9 @@ const router = express.Router();
 const authController = require('../controllers/authController');
 const AnnouncementController = require('../controllers/announcementController');
 
-router.post('/', authController.protect,authController.restrictTo('admin', 'superadmin') ,AnnouncementController.createAnnouncement);
+router.post('/', authController.protect, authController.restrictTo('admin', 'superadmin'), AnnouncementController.createAnnouncement);
 router.get('/', AnnouncementController.getAllAnnouncements);
 router.patch('/', authController.protect, authController.restrictTo('admin', 'superadmin'), AnnouncementController.updateAnnouncement);
-router.delete('/', AnnouncementController.protect, AnnouncementController.restrictTo('admin', 'superadmin'), AnnouncementController.deleteAnnouncement);
+router.delete('/', authController.protect, authController.restrictTo('admin', 'superadmin'), AnnouncementController.deleteAnnouncement);
 
 module.exports = router
