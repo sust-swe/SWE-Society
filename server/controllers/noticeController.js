c63302484499a5113668eea76c342aec302cc65d
RakibulRanak <rakibulhasanranak1@gmail.com>
2021-02-09 03:37:05 +0600

    h

--- server/controllers/noticeController.js
+++ server/controllers/noticeController.js
@@ -4,27 +4,28 @@ const AppError = require('../utils/appError');
 
 
 exports.createNotice = catchAsync(async (req, res, next) => {
+    console.log("hello")
     req.body.hidden = undefined;
-    const Notice = await Notice.create(req.body);
-    res.status(200).json(Notice);
+    const notice = await Notice.create(req.body);
+    res.status(200).json(notice);
 });
 
 exports.updateNotice = catchAsync(async (req, res, next) => {
     req.body.hidden = undefined;
-    const Notice = await Notice.update(req.body, { returning: true, where: { id: req.params.id } });
-    if (Notice[0] == 0)
+    const notice = await Notice.update(req.body, { returning: true, where: { id: req.params.id } });
+    if (notice[0] == 0)
         return next(new AppError(`Not found`, 404));
-    res.status(200).json(Notice);
+    res.status(200).json(notice);
 });
 
 exports.deleteNotice = catchAsync(async (req, res, next) => {
-    const Notice = await Notice.update({ hidden: "true" }, { returning: true, where: { id: req.params.id } });
-    if (Notice)
+    const notice = await Notice.update({ hidden: "true" }, { returning: true, where: { id: req.params.id } });
+    if (notice)
         return next(new AppError(`Not found`, 404));
     res.status(200).json({ message: "Successfully deleted" });
 });
 
 exports.getAllNotices = catchAsync(async (req, res, next) => {
-    const Notices = await Notice.findAll({ where: { hidden: "false" } });
-    res.status(200).json(Notices);
+    const notices = await Notice.findAll({ where: { hidden: "false" } });
+    res.status(200).json(notices);
 })
